78de2059f26e5b0f65097483e419fa3c79b7d3b0
Windrider <christianlualemosc@hotmail.com>
2026-09-23 12:13:53 -0300

    g

--- src/routes/local/product.ts
+++ src/routes/local/product.ts
@@ -8,16 +8,34 @@ export default (fastify: FastifyInstance) => {
   }>('/local/products/query', {
   }, async (req, res) => {
     const token = req.session.storeToken
-        
-        const response = await MakeWsRequest(token, {
-          method: "GET",
-          url: "/products/query?query=" + req.query.query,
-        })
-        if (response.payload.status >= 400) {
-          return res.code(response.payload.status).send({ message: response.payload.error || "Erro ao criar auditoria" })
-        }
-        else {
-          return response.payload.body
-        }
+
+    const response = await MakeWsRequest(token, {
+      method: "GET",
+      url: "/products/query?query=" + req.query.query,
+    })
+    if (response.payload.status >= 400) {
+      return res.code(response.payload.status).send({ message: response.payload.error || "Erro ao pesquisar produtos" })
+    }
+    else {
+      return response.payload.body
+    }
+  })
+
+  fastify.get<{
+    Querystring: { query: string }
+  }>('/local/products/query-name', {
+  }, async (req, res) => {
+    const token = req.session.storeToken
+
+    const response = await MakeWsRequest(token, {
+      method: "GET",
+      url: "/products/query-name?query=" + req.query.query,
+    })
+    if (response.payload.status >= 400) {
+      return res.code(response.payload.status).send({ message: response.payload.error || "Erro ao pesquisar produtos" })
+    }
+    else {
+      return response.payload.body
+    }
   })
 }
